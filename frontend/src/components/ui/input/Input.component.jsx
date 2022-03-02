aa0c2b80edbe636df9d358b5beef4e986d28e9a0
Dean <dean@deansantillan.com>
2022-03-02 23:07:42 +1000

    update(): adding 2 more files to the commit that I forgot.

---
+++ frontend/src/components/ui/input/Input.component.jsx
@@ -0,0 +1,88 @@
+import { useReducer, useEffect } from 'react';
+import { validate } from '../../../util/validator.utility';
+import './Input.styles.scss';
+
+const inputReducer = (state, action) => {
+	switch (action.type) {
+		case 'CHANGE':
+			return {
+				...state,
+				value: action.val,
+				isValid: validate(action.val, action.validators),
+			};
+		case 'TOUCH':
+			return {
+				...state,
+				isTouched: true,
+			};
+		default:
+			return state;
+	}
+};
+
+const Input = ({
+	id,
+	type,
+	placeholder,
+	label,
+	rows,
+	element,
+	errorText,
+	validators,
+    onInput
+}) => {
+	const [inputState, dispatch] = useReducer(inputReducer, {
+		value: '',
+		isTouched: false,
+		isValid: false,
+	});
+    const {value, isValid} = inputState
+	//
+	useEffect(() => {
+		onInput(id, value, isValid);
+	}, [id, onInput,value, isValid]);
+	//
+	const changeHandler = event => {
+		dispatch({ type: 'CHANGE', val: event.target.value, validators });
+	};
+	//
+	const touchHandler = () => {
+		dispatch({ type: 'TOUCH' });
+	};
+	//
+	const formElement =
+		element === 'input' ? (
+			<input
+				id={id}
+				type={type}
+				placeholder={placeholder}
+				onChange={changeHandler}
+				onBlur={touchHandler}
+				value={inputState.value}
+			/>
+		) : (
+			<textarea
+				id={id}
+				rows={rows || 3}
+				onChange={changeHandler}
+				onBlur={touchHandler}
+				value={inputState.value}
+			/>
+		);
+	//
+	return (
+		<div
+			className={`form-control ${
+				!inputState.isValid &&
+				inputState.isTouched &&
+				'form-control--invalid'
+			}`}
+		>
+			<label htmlFor={id}>{label}</label>
+			{formElement}
+			{!inputState.isValid && inputState.isTouched && <p>{errorText}</p>}
+		</div>
+	);
+};
+
+export default Input;
